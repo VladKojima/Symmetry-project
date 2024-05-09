1d4c21cf048044f1fba6986cd38085417eec9311
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-05-09 20:48:09 +0400

    init + backend

---
+++ backend/src/main/java/team/symmetry/ResumeBack/ResumeBackApplication.java
@@ -0,0 +1,13 @@
+package team.symmetry.ResumeBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeBackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/team/symmetry/ResumeBack/ResumeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package team.symmetry.ResumeBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
