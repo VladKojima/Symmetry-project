0db98476137fb65c57c45888959d0313e23ced4d
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-05-10 15:51:50 +0400

    back: remove placeholder test

--- backend/src/test/java/team/symmetry/ResumeBack/ResumeBackApplicationTests.java
+++ backend/src/test/java/team/symmetry/ResumeBack/ResumeBackApplicationTests.java
@@ -1,13 +1,8 @@
 package team.symmetry.ResumeBack;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ResumeBackApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
