c1d9984586b003b4383bbe3083459675fc414672
maria23 <qubik15@gmail.com>
2024-05-14 14:14:16 +0400

    upd

--- authserver/src/main/java/com/jwt/service/config/security/SecurityConfig.java
+++ authserver/src/main/java/com/jwt/service/config/security/SecurityConfig.java
@@ -48,9 +48,9 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .addFilterBefore(corsFilter(), CookieFilter.class)
                                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/js/**", "/test/**").permitAll()
-                                .requestMatchers("api/auth/logout").authenticated()
                                 .requestMatchers("api/login/**", "api/auth/**", "api/user")
                                 .hasRole("ANONYMOUS")
+                                .requestMatchers("api/auth/logout").authenticated()
                                 .anyRequest().authenticated()
                 );
         return http.build();
