6c3fa0bcf4d7a67f99dcad949ba3c860038b7e4a
VladKojima <102826875+VladKojima@users.noreply.github.com>
2024-05-15 04:56:31 +0400

    back: moderator construct

--- backend/src/main/java/team/symmetry/ResumeBack/models/Moderator.java
+++ backend/src/main/java/team/symmetry/ResumeBack/models/Moderator.java
@@ -16,13 +16,17 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "moderators")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Moderator {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
